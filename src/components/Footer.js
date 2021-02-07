5a99f56f396156888bd4e03e8539bf73e8b67c57
Merlin Rock <rock.merlin@yahoo.com>
2021-02-07 15:05:28 -0500

    Added a Header and Footer component

---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+import React from "react";
+import './Footer.css';
+
+
+function Footer() {
+    return (
+        <div>
+            <footer class="hero-foot page-footer fixed-bottom font-small light bg-primary pt-1" />
+            <section class="footer-copyright text-center py-0">© 2020 Copyright:</section>
+        </div>
+    )
+}
+
+export default Footer;
--- src/components/Header.js
+++ src/components/Header.js
@@ -13,40 +13,42 @@ import './Header.css';
 
 function Header() {
     return (
-        
-        
-        // <section className="navbar-brand">
-          
-            <img src={logo} className="logo"/>
-          
-    
-        //   <a role="button" className="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
-        //     <span aria-hidden="true"></span>
-        //     <span aria-hidden="true"></span>
-        //     <span aria-hidden="true"></span>
-        //   </a>
-        // </section>
-    
-        // <section id="navbarBasicExample" className="navbar-menu">
-        //   <section className="navbar-start">
-        //     <a className="navbar-item" href="index.html">
-        //       Home/About Me
-        //     </a>
-    
-        //     <a className="navbar-item" href="portfolio.html">
-        //       Portfolio
-        //     </a>
-    
-        //     <a className="navbar-item" href="contact.html">
-        //       Contact
-        //     </a>
-    
-        //     <a className="navbar-item"
-        //       href="https://docs.google.com/document/d/18z3xuPjWtQcAxq-XY8BxMsd8359yB9sT--OMthnhTt8/edit">
-        //       My Resume
-        //     </a>
-        //   </section>
-        // </section>
+        <div>
+          <nav className="navbar" role="navigation" aria-label="main navigation">
+            <Section className="navbar-brand">
+              <a className="">
+                <img src="./Assets/images/Rok Computing Logo.png" className="logo" />
+              </a>
+
+              <a role="button" className="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+                <span aria-hidden="true"></span>
+                <span aria-hidden="true"></span>
+                <span aria-hidden="true"></span>
+              </a>
+            </Section>
+
+            <Section id="navbarBasicExample" className="navbar-menu">
+              <Section className="navbar-start">
+                <a className="navbar-item" href="index.html">
+                  Home/About Me
+                </a>
+
+                <a className="navbar-item" href="portfolio.html">
+                  Portfolio
+                </a>
+
+                <a className="navbar-item" href="contact.html">
+                  Contact
+                </a>
+
+                <a className="navbar-item"
+                  href="https://docs.google.com/document/d/18z3xuPjWtQcAxq-XY8BxMsd8359yB9sT--OMthnhTt8/edit">
+                  My Resume
+                </a>
+              </Section>
+            </Section>
+          </nav>
+        </div>
       
       
     )
